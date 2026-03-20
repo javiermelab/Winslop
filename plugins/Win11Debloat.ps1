6f96a44efd58332ae63d688a2d4b7651c5c022c8
Belim <belim@builtbybel.com>
2026-03-20 10:21:47 +0100

    Add Raphire's Win11Debloat to plugins

---
+++ plugins/Win11Debloat.ps1
@@ -0,0 +1,6 @@
+﻿# Win11Debloat Launcher.ps1
+[Commands]
+Info=Launches Raphire's Win11Debloat using the official remote bootstrap.
+Check=echo Ready
+Do=powershell -NoProfile -ExecutionPolicy Bypass -Command "& ([scriptblock]::Create((irm 'https://debloat.raphi.re/')))"
+Undo=echo No undo available
